38d2bbd5ad129e0cfda76cb207bd0e3cd07a3c3e
Sara Jamali <109460041+sarajamali91@users.noreply.github.com>
2023-02-21 17:34:05 +0100

    Add files via upload
    This code defines a list of words, shuffles them randomly, and then displays each word one at a time, asking the user to type in the word they see. After the user enters their answer, the code checks if it matches the displayed word and prints out a message indicating whether the user was correct or not. The code also pauses for 1 second between each word to give the user time to respond. This simple program could be used to assess working memory and attention abilities.

---
+++ Machine Learning/cognitiv.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import the necessary libraries
+import random
+import time
+
+# Define a list of words
+words = ['cat', 'dog', 'fish', 'bird', 'rabbit']
+
+# Shuffle the list of words
+random.shuffle(words)
+
+# Display each word in the shuffled list and ask the user to type it in
+for word in words:
+    print(word)
+    user_input = input('Please type the word you see above: ')
+    
+    # Check if the user's input matches the displayed word
+    if user_input == word:
+        print('Correct!')
+    else:
+        print('Incorrect.')
+    
+    # Pause for 1 second before displaying the next word
+    time.sleep(1)
+
+
+# In[ ]:
+
+
+
+
